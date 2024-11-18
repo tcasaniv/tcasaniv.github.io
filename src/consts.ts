3d8cfefc2c748a2efc7e56d8113e7f837974e770
Terry Casani Valencia <99104321+tcasaniv@users.noreply.github.com>
2024-11-18 09:52:15 -0500

    Update to tcasaniv.github.io

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BRAND = "tcasaniv";
-export const SITE_TITLE = "tcasaniv";
-export const SITE_URL = "https://tcasaniv.github.io/tcasaniv-web";
+export const BRAND = "tcasaniv.github.io";
+export const SITE_TITLE = "tcasaniv.github.io";
+export const SITE_URL = "https://tcasaniv.github.io/";
 export const SITE_DESCRIPTION = "Sitio web sobre temas relacionados a la ingeniería";
 export const PAGINATION_POSTS_PER_PAGE = 5;
